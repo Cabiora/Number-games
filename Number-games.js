daabc5a442898254b83cc4086bb51b323d346471
Cabiora <146395932+Cabiora@users.noreply.github.com>
2024-12-05 19:26:26 -0500

    Update Number-games.js

--- Number-games.js
+++ Number-games.js
@@ -1,1 +1,5 @@
-console.log("Bonjour, monde !");
+function startGame() {
+    const difficulty = document.getElementById('difficulty').value;
+    localStorage.setItem('maxNumber', difficulty);
+    window.location.href = 'game.html';
+}
