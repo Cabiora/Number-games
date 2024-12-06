751451b2e408cc1d22ea28e442df4104361bd5ff
Cabiora <146395932+Cabiora@users.noreply.github.com>
2024-12-06 01:56:03 -0500

    Delete Number-games.js

--- Number-games.js
+++
@@ -1,5 +0,0 @@
-function startGame() {
-    const difficulty = document.getElementById('difficulty').value;
-    localStorage.setItem('maxNumber', difficulty);
-    window.location.href = 'game.html';
-}
